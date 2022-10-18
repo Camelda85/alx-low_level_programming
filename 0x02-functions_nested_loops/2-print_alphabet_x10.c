1c269d0204cd6c965285f842fbc9072237da7628
Camelda85 <baggy9ja@gmail.com>
2022-10-18 11:55:29 -0700

    10 times lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - print alpha 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char alpha;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+
+		_putchar(10);
+	}
+}
